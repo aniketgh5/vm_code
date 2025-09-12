058e073c0f1c0a7e1ec219d64644ebb07033695b
aniketgh5 <checkaniket@gmail.com>
2025-09-12 21:55:06 +0530

    code2

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "kolkata-resource"
-    storage_account_name = "kolkata7401"
-    container_name       = "kolkata-container"
-    key                  = "kolkata.tfstate"
-    # Optional: Use Azure AD authentication instead of access keys
-    # use_azuread_auth = true 
-    # Optional: If using Azure AD authentication with OIDC
-    # use_oidc = true 
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "kolkata-resource"
+#     storage_account_name = "kolkata7401"
+#     container_name       = "kolkata-container"
+#     key                  = "kolkata.tfstate"
+#     # Optional: Use Azure AD authentication instead of access keys
+#     # use_azuread_auth = true 
+#     # Optional: If using Azure AD authentication with OIDC
+#     # use_oidc = true 
+#   }
+# }
